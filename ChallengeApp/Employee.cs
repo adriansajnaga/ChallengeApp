09d225f29004a6e7e2698b75da8ee8517c7c37ba
Adrian Sajnaga <adrian.sajnaga@outlook.com>
2023-08-26 01:10:02 +0200

    Homework - day 6

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace ChallengeApp
+{
+    public class Employee                                           //KLASA
+    {
+        private List<int> listOfPoints = new List<int>();
+         public Employee(string name, string surname, int age)      //KONSTRUKTOR
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public void AddPoints(int points)                           //METODA KLASY
+        {
+            this.listOfPoints.Add(points);
+        }
+
+        public int SumOfPoints
+        {
+            get
+            {
+                return this.listOfPoints.Sum();
+            }
+        }                 
+        
+    }
+}
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,22 +1,39 @@
-﻿int number = 45666;
-string numberToString = number.ToString();
-char[] letters = numberToString.ToCharArray();
-int existingLetter = 0;
+﻿using ChallengeApp;
 
-for (int i = 0; i <= 9; i++) 
+Employee employee1 = new Employee("Adrian","Sajnaga",36);
+List<int> employee1Points = [1, 4, 8, 10, 6];
+Employee employee2 = new Employee("Michał", "Prorok", 41);
+List<int> employee2Points = [5, 3, 10, 10, 5];
+Employee employee3 = new Employee("Przemek", "Wolański", 27);
+List<int> employee3Points = [7, 1, 2, 2, 3];
+List<Employee> employees = [employee1, employee2, employee3];
+
+var maxPoints = 0;
+Employee winner = null;
+
+foreach(int points in employee1Points)
 {
-    foreach (char letter in letters)
-    {
-        int letterToInt = Convert.ToInt32(letter.ToString());
-        if (letterToInt == i)
-        {
-            existingLetter++;
-        }
-    }
-    Console.WriteLine($"{i} => {existingLetter}");
-    existingLetter = 0;
+    employee1.AddPoints(points);
 }
 
+foreach (int points in employee2Points)
+{
+    employee2.AddPoints(points);
+}
 
+foreach (int points in employee3Points)
+{
+    employee3.AddPoints(points);
+}
 
-//int letterToInt = letter - '0';   inna metoda zamiany cyfry zapisanej w kodzie ASCII na cyfrę
+foreach (var employee in employees)
+{
+        if (employee.SumOfPoints>maxPoints)
+        {
+           maxPoints = employee.SumOfPoints;
+           winner = employee;
+        }
+}
+
+Console.WriteLine($"The best employee of this month is: " + winner.Name + " " +winner.Surname);
+Console.WriteLine("He is " + winner.Age + " and has earned a total of " + maxPoints + " points!");
