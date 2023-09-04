9cd0ebb3e4a3adcb8e4cd139ce8a572959ac9895
Adrian Sajnaga <adrian.sajnaga@outlook.com>
2023-09-04 22:20:11 +0200

    Homework - day 11

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChallengeApp
 {
-    public class Employee                                          
+    public class Employee
     {
 
         private List<float> listOfPoints = new List<float>();
@@ -17,7 +17,7 @@
 
         public void AddPoints(float points)
         {
-            if(points>=0 && points<=100)
+            if (points >= 0 && points <= 100)
             {
                 this.listOfPoints.Add(points);
             }
@@ -25,10 +25,10 @@
             {
                 Console.WriteLine($"Invalid value: {points} - out of range");
             }
-            
+
         }
 
-        public void AddPoints(double points) 
+        public void AddPoints(double points)
         {
             float result = (float)points;
             this.AddPoints(result);
@@ -48,17 +48,17 @@
 
         public void AddPoints(string points)
         {
-            if(float.TryParse(points, out float result))
+            if (float.TryParse(points, out float result))
             {
                 this.AddPoints(result);
             }
-            else 
+            else
             {
-                Console.WriteLine($"Invalid value: {points} - not float" );
+                Console.WriteLine($"Invalid value: {points} - not float");
             }
         }
 
-        public Statistics GetStatistics()
+        public Statistics GetStatisticsWithForEach()
         {
             var statistics = new Statistics();
 
@@ -66,15 +66,76 @@
             statistics.Min = float.MaxValue;
             statistics.Avg = 0;
 
-            foreach (var point in this.listOfPoints) 
+            foreach (var point in this.listOfPoints)
             {
                 statistics.Max = Math.Max(statistics.Max, point);
                 statistics.Min = Math.Min(statistics.Min, point);
                 statistics.Avg += point;
             }
-                statistics.Avg /= this.listOfPoints.Count;
-
+            statistics.Avg /= this.listOfPoints.Count;
             return statistics;
         }
-}
+
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics = new Statistics();
+
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Avg = 0;
+            var index = 0;
+
+            do
+            {
+                statistics.Max = Math.Max(statistics.Max, this.listOfPoints[index]);
+                statistics.Min = Math.Min(statistics.Min, this.listOfPoints[index]);
+                statistics.Avg += this.listOfPoints[index];
+                index++;
+            }
+            while (index < this.listOfPoints.Count);
+
+            statistics.Avg /= this.listOfPoints.Count;
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics = new Statistics();
+
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Avg = 0;
+            var index = 0;
+
+            while (index < this.listOfPoints.Count)
+            {
+                statistics.Max = Math.Max(statistics.Max, this.listOfPoints[index]);
+                statistics.Min = Math.Min(statistics.Min, this.listOfPoints[index]);
+                statistics.Avg += this.listOfPoints[index];
+                index++;
+            }
+            statistics.Avg /= this.listOfPoints.Count;
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics = new Statistics();
+
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Avg = 0;
+
+            for (int index = 0; index < this.listOfPoints.Count; index++)
+            {
+                statistics.Max = Math.Max(statistics.Max, this.listOfPoints[index]);
+                statistics.Min = Math.Min(statistics.Min, this.listOfPoints[index]);
+                statistics.Avg += this.listOfPoints[index];
+            }
+            statistics.Avg /= this.listOfPoints.Count;
+            return statistics;
+        }
+
+
+    }
 }
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -10,9 +10,24 @@ foreach(int points in employee1Points)
 
 employee1.AddPoints("100");
 employee1.AddPoints("Test");
+Console.WriteLine("");
 
-var statistics = employee1.GetStatistics();
+var statistics = employee1.GetStatisticsWithForEach();
+Console.WriteLine("Pętla foreach");
+Console.WriteLine($"Maksymalna ocena: {statistics.Max} | Minimalna ocena: {statistics.Min} | Średnia: {statistics.Avg:N2}");
+Console.WriteLine("");
 
+var statistics2 = employee1.GetStatisticsWithDoWhile();
+Console.WriteLine("Pętla do while");
+Console.WriteLine($"Maksymalna ocena: {statistics.Max} | Minimalna ocena: {statistics.Min} | Średnia: {statistics.Avg:N2}");
+Console.WriteLine("");
 
+var statistics3 = employee1.GetStatisticsWithWhile();
+Console.WriteLine("Pętla while");
+Console.WriteLine($"Maksymalna ocena: {statistics.Max} | Minimalna ocena: {statistics.Min} | Średnia: {statistics.Avg:N2}");
+Console.WriteLine("");
+
+var statistics4 = employee1.GetStatisticsWithFor();
+Console.WriteLine("Pętla for");
 Console.WriteLine($"Maksymalna ocena: {statistics.Max} | Minimalna ocena: {statistics.Min} | Średnia: { statistics.Avg:N2}");
 Console.ReadLine();
